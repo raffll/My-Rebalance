31c0bf45ace88237a6dbeec0f05ae267470e40a7
raffll <rafal.wierzchos@gmail.com>
2025-07-03 13:06:25 +0200

    Update .build.sh

--- .build.sh
+++ .build.sh
@@ -6,4 +6,4 @@ if [ -e "$NAME.zip" ]; then
 	rm "$NAME.zip"
 fi
 
-"C:\Program Files\7-Zip\7z.exe" a -tzip "$NAME.zip" "*.esp" README.md
+"C:\Program Files\7-Zip\7z.exe" a -tzip "$NAME.zip" "*.esp" README.md CHANGELOG.md
